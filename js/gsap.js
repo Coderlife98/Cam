7408334e453ef9a3c9cc31f69b4d022fb83309bb
sunny <anita.hr112@gmail.com>
2024-05-06 16:21:48 +0530

    All ml files

--- js/gsap.js
+++ js/gsap.js
@@ -14,11 +14,6 @@ gsap.from(".set-desi", {
 });
 // for home page end
 
-
-
-
-
-
 gsap.from(".ggg-aa", {
     y: -2000,
     duration: 2,
@@ -49,106 +44,143 @@ gsap.from(".gsap-ri", {
 
 var tl = gsap.timeline();
 tl.from(".point-software", { x: -500, duration: 2 });
-
-
-
-
-
-// mobile app start
-gsap.from(".m1gsap",{
-   x:-500,
-    duration:1,
-    delay:2,
+gsap.from(".m1gsap", {
+    x: -500,
+    duration: 1,
+    delay: 2,
     stagger: true,
-    scrollTrigger:{
-        trigger:".m1gsap",
-        scroller:"body",
-        scrub:2
+    scrollTrigger: {
+        trigger: ".m1gsap",
+        scroller: "body",
+        scrub: 2
     }
 });
 
-gsap.from("#m1gsapright",{
-    x:800,
-    duration:1,
-    delay:2
+gsap.from("#m1gsapright", {
+    x: 800,
+    duration: 1,
+    delay: 2
 });
 
-gsap.from(".app-box",{
-    x:-2000,
-    duration:2,
-    delay:2,
-    scrollTrigger:{
-        trigger:".app-box",
-        scroller:"body",
-        scrub:2
+gsap.from(".app-box", {
+    x: -2000,
+    duration: 2,
+    delay: 2,
+    scrollTrigger: {
+        trigger: ".app-box",
+        scroller: "body",
+        scrub: 2
     }
 });
 
 
-gsap.from(".soft1",{
-    x:200,
-    duration:1,
-    scale:1,
-    scrollTrigger:{
-        trigger:".soft1",
-        scroller:"body",
-        scrub:2
+gsap.from(".soft1", {
+    x: 200,
+    duration: 1,
+    scale: 1,
+    scrollTrigger: {
+        trigger: ".soft1",
+        scroller: "body",
+        scrub: 2
     }
 });
 
-gsap.from(".bg-back",{
-    duration:1,
-    x:300,
-    scrollTrigger:{
-        trigger:".bg-back",
-        scroller:"body",
-        scrub:2
+gsap.from(".bg-back", {
+    duration: 1,
+    x: 300,
+    scrollTrigger: {
+        trigger: ".bg-back",
+        scroller: "body",
+        scrub: 2
     }
 })
 
-gsap.from(".mmm,.about-microfi",{
-    duration:1,
-    opacity:0,
-    scrollTrigger:{
-        trigger:".mmm,.about-microfi",
-        scroller:"body",
-        scrub:3
+gsap.from(".mmm,.about-microfi", {
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".mmm,.about-microfi",
+        scroller: "body",
+        scrub: 3
     }
 })
 
-gsap.from(".set-last-txt",{
-    duration:0.3,
-    opacity:0,
-    scrollTrigger:{
-        trigger:".set-last-txt",
-        scroller:"body",
-        scrub:2
+gsap.from(".set-last-txt", {
+    duration: 0.3,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".set-last-txt",
+        scroller: "body",
+        scrub: 2
     }
 })
 
-gsap.from(".gss",{
-    opacity:0,
-    duration:1,
+gsap.from(".gss", {
+    opacity: 0,
+    duration: 1,
     // rotate:360,
-    scrollTrigger:{
-        trigger:".gss",
-        scroller:"body",
-        scrub:3
+    scrollTrigger: {
+        trigger: ".gss",
+        scroller: "body",
+        scrub: 3
     }
 })
 // mobile app end
 
 // nbfc  start
-gsap.from(".nb-animate",{
-    
-    duration:2,
-    opacity:0,
-    scale:1.5,
-   
-    scrollTrigger:{
-        trigger:".nb-animate",
-        scroller:"body",
-        scrub:2
+gsap.from(".nb-animate", {
+    duration: 2,
+    opacity: 0,
+    scale: 1.5,
+
+    scrollTrigger: {
+        trigger: ".nb-animate",
+        scroller: "body",
+        scrub: 2
+    }
+});
+
+
+// nbfc  end
+
+gsap.from(".tool", {
+    duration: 2,
+    scale: 1.5,
+    opacity: 1,
+    scrollTrigger: {
+        trigger: ".tool",
+        scroller: "body",
+        scrub: 4,
     }
 })
-// nbfc  end
+
+gsap.from(".butt", {
+    duration: 2,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".butt",
+        scroller:"body",
+        scrub:4
+    }
+})
+
+gsap.from(".pos-sect-1-head",{
+    duration:2,
+    opacity:0,
+   
+    scrollTrigger:{
+        trigger:".pos-sect-1-head",
+        scroller:"body",
+        scrub:3
+    }
+});
+
+gsap.from(".poin-eff",{
+    
+   x:500,
+    scrollTrigger:{
+        trigger:".poin-eff",
+        scroller:"body",
+        scrub:3
+    }
+})
